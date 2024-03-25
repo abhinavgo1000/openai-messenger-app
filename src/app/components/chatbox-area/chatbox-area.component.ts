4141d78d239760cfbf80ebcc4ce29d1e2c6946e3
Abhinav Goel <abhinavgo1000@gmail.com>
2024-03-25 09:12:07 -0400

    angular service for chat messenger added

--- src/app/components/chatbox-area/chatbox-area.component.ts
+++ src/app/components/chatbox-area/chatbox-area.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { OpenaiChatService } from '../../shared/services/openai-chat.service'
 
 @Component({
   selector: 'app-chatbox-area',
@@ -7,6 +10,39 @@ import { Component } from '@angular/core';
   templateUrl: './chatbox-area.component.html',
   styleUrl: './chatbox-area.component.scss'
 })
-export class ChatboxAreaComponent {
+export class ChatboxAreaComponent implements OnInit, OnDestroy {
 
+  messages: any[] = [];
+  newMessage: string = '';
+  private messageSubscription: Subscription;
+
+  constructor(private chatService: OpenaiChatService) {}
+
+  ngOnInit() {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageSubscription = this.chatService.getMessages().subscribe({
+      next: (data: any) => this.messages = data,
+      error: (err: any) => console.error(err) // Handle errors appropriately in real app
+    });
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.newMessage).subscribe({
+      next: (response) => {
+        // Optionally handle response
+        this.newMessage = ''; // Reset the input field
+        this.loadMessages();  // Reload messages to include the new one
+      },
+      error: (err: any) => console.error(err) // Handle errors appropriately in real app
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.messageSubscription) {
+      this.messageSubscription.unsubscribe();
+    }
+  }
 }
