ba69aa1536106e628e695202c1ce0713c5b26c6c
Abhinav Goel <abhinavgo1000@gmail.com>
2024-03-26 10:17:06 -0400

    withFetch added to httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withFetch } from '@angular/common/http'
 import { provideStore } from '@ngrx/store';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideStore(), provideAnimationsAsync(), provideEffects()]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideStore(), provideAnimations(), provideAnimationsAsync(), provideEffects()]
 };
