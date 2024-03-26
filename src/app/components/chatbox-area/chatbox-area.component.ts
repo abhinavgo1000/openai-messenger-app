ab1782fe66b8ba4314877bdcb3cd65deb14de3e0
Abhinav Goel <abhinavgo1000@gmail.com>
2024-03-26 11:42:13 -0400

    typewriter effect added on the received message

--- src/app/components/chatbox-area/chatbox-area.component.ts
+++ src/app/components/chatbox-area/chatbox-area.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  OnDestroy, 
+  AfterViewInit,
+  ElementRef,
+  Input,
+  Renderer2,
+  ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
@@ -21,9 +29,17 @@ import { OpenaiChatService } from '../../shared/services/openai-chat.service'
   templateUrl: './chatbox-area.component.html',
   styleUrl: './chatbox-area.component.scss'
 })
-export class ChatboxAreaComponent implements OnInit, OnDestroy {
+export class ChatboxAreaComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  messages: any[] = [];
+  @ViewChild('textElement') textElement: ElementRef;
+  @ViewChild('blinkElement') blinkElement: ElementRef;
+
+  @Input() messages: string[] = [];
+  @Input() textColor: 'black';
+  @Input() fontSize: '20px';
+  @Input() blinkWidth = '2px';
+  @Input() typingSpeedMilliseconds = 300;
+
   newMessage: string = '';
   private messageSubscription: Subscription;
   floatLabelControl = new FormControl('auto' as FloatLabelType);
@@ -31,9 +47,12 @@ export class ChatboxAreaComponent implements OnInit, OnDestroy {
     floatLabel: this.floatLabelControl,
   });
 
+  private i = 0;
+
   constructor(
     private chatService: OpenaiChatService,
-    private _formBuilder: FormBuilder) {}
+    private _formBuilder: FormBuilder,
+    private renderer: Renderer2) {}
 
   ngOnInit() {
     this.loadMessages();
@@ -55,6 +74,54 @@ export class ChatboxAreaComponent implements OnInit, OnDestroy {
       },
       error: (err: any) => console.error(err) // Handle errors appropriately in real app
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.initVariables();
+    this.typingEffect();
+  }
+
+  private initVariables(): void {
+    this.renderer.setStyle(
+      this.textElement.nativeElement,
+      'color',
+      this.textColor
+    );
+    this.renderer.setStyle(
+      this.textElement.nativeElement,
+      'font-size',
+      this.fontSize
+    );
+    this.renderer.setStyle(this.textElement.nativeElement, 'padding', '0.1em');
+
+    this.renderer.setStyle(
+      this.blinkElement.nativeElement,
+      'border-right-width',
+      this.blinkWidth
+    );
+    this.renderer.setStyle(
+      this.blinkElement.nativeElement,
+      'border-right-color',
+      this.textColor
+    );
+    this.renderer.setStyle(
+      this.blinkElement.nativeElement,
+      'font-size',
+      this.fontSize
+    );
+  }
+
+  private typingEffect(): void {
+    const word = this.messages[this.i].split('');
+    const loopTyping = () => {
+      if (word.length > 0) {
+        this.textElement.nativeElement.innerHTML += word.shift();
+      } else {
+        return;
+      }
+      setTimeout(loopTyping, this.typingSpeedMilliseconds);
+    };
+    loopTyping();
   }
 
   getFloatLabelValue(): FloatLabelType {
