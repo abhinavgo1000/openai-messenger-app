ac864b334aeefeab3310229631ee5e00701b74a4
Abhinav Goel <abhinavgo1000@gmail.com>
2024-03-27 15:56:17 -0400

    code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HomePageComponent } from './components/home-page/home-page.component'
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { PageFooterComponent } from './components/page-footer/page-footer.component';
 
@@ -10,7 +9,6 @@ import { PageFooterComponent } from './components/page-footer/page-footer.compon
   standalone: true,
   imports: [
     RouterOutlet, 
-    HomePageComponent,
     PageHeaderComponent,
     PageFooterComponent],
   templateUrl: './app.component.html',
