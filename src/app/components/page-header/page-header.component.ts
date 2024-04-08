b546e8dc469d17f7a4bf8ed1c6d2722b8e97be67
Abhinav Goel <abhinavgo1000@gmail.com>
2024-04-08 14:21:19 -0400

    search bar added to page header and mobile page header configured

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -1,10 +1,13 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DOCUMENT } from '@angular/common';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
 
 import * as LikeActions from '../../store/like/like.actions';
 import * as LikeSelectors from '../../store/like/like.selector';
@@ -14,10 +17,14 @@ import { AppState } from '../../store/app.state';
   selector: 'app-page-header',
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatBadgeModule
   ],
   templateUrl: './page-header.component.html',
   styleUrl: './page-header.component.scss'
@@ -25,6 +32,11 @@ import { AppState } from '../../store/app.state';
 export class PageHeaderComponent {
 
 	isActive: boolean;
+
+  toggleSearch = false;
+  searchText = '';
+
+  @ViewChild('searchbar') searchbar: ElementRef;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -50,27 +62,17 @@ export class PageHeaderComponent {
     this.document.location.href = 'https://www.linkedin.com/in/abhinav-goel-41a87a20b/';
   }
 
-  shareOnFacebook(): void {
-    var url = 'http://www.facebook.com/sharer.php';
-    url += '?u=' + encodeURIComponent(window.location.href);
-    this.popup(url);
+  navigateToProfile() {}
+
+  navigateToAccount() {}
+
+  openSearchBar() {
+    this.toggleSearch = true;
+    this.searchbar.nativeElement.focus();
   }
 
-  shareOnTwitter(): void {
-    var url = 'http://twitter.com/share?';
-    url += 'text=' + encodeURIComponent(document.title);
-    url += '&url=' + encodeURIComponent(window.location.href);
-    url += '&counturl=' + encodeURIComponent(window.location.href);
-    this.popup(url);
-  }
-
-  shareOnLinkedin(): void {
-    var url = 'https://www.linkedin.com/sharing/share-offsite/';
-    url += '?u=' + encodeURIComponent(window.location.href);
-    this.popup(url);
-  }
-
-  popup(url: string) {
-    window.open(url, '', 'toolbar=0,status=0,width=626, height=436');
+  closeSearchBar() {
+    this.searchText = '';
+    this.toggleSearch = false;
   }
 }
