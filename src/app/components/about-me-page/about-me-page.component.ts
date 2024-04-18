6146bc807ecfb86c4d9b5651cf575ace58398cc6
Abhinav Goel <abhinavgo1000@gmail.com>
2024-04-18 08:37:50 -0400

    translate toolbar added to about-me page

--- src/app/components/about-me-page/about-me-page.component.ts
+++ src/app/components/about-me-page/about-me-page.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { TranslateToolbarComponent } from '../translate-toolbar/translate-toolbar.component';
+
+import { DynamicTranslationDirective } from '../../shared/directives/dynamic-translation.directive';
 
 @Component({
   selector: 'app-about-me-page',
   standalone: true,
   imports: [
     MatCardModule,
-    MatExpansionModule
+    MatExpansionModule,
+    TranslateModule,
+    TranslateToolbarComponent,
+    DynamicTranslationDirective
   ],
   templateUrl: './about-me-page.component.html',
   styleUrl: './about-me-page.component.scss'
 })
 export class AboutMePageComponent {
+
   panelOpenState = false;
 
-  constructor() {}
+  constructor(private translate: TranslateService) {}
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
