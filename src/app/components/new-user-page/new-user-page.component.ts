9ebf3a466baf958ba3cb6970fd4f1183f6e83335
Abhinav Goel <abhinavgo1000@gmail.com>
2024-04-18 09:24:02 -0400

    signup page congigured

--- src/app/components/new-user-page/new-user-page.component.ts
+++ src/app/components/new-user-page/new-user-page.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { UserSignupComponent } from '../user-signup/user-signup.component';
+import { TranslateToolbarComponent } from '../translate-toolbar/translate-toolbar.component';
+
+import { DynamicTranslationDirective } from '../../shared/directives/dynamic-translation.directive';
 
 @Component({
   selector: 'app-new-user-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    TranslateModule,
+    UserSignupComponent,
+    TranslateToolbarComponent,
+    DynamicTranslationDirective
+  ],
   templateUrl: './new-user-page.component.html',
   styleUrl: './new-user-page.component.scss'
 })
 export class NewUserPageComponent {
 
+  constructor(private translate: TranslateService) {}
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
