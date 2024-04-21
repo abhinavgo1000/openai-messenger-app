ce146b6e5d075307b234210699815fceb6eaef07
Abhinav Goel <abhinavgo1000@gmail.com>
2024-04-20 20:34:20 -0400

    new user success page configured

--- src/app/components/new-user-success-page/new-user-success-page.component.ts
+++ src/app/components/new-user-success-page/new-user-success-page.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { TranslateToolbarComponent } from '../translate-toolbar/translate-toolbar.component';
+
+import { DynamicTranslationDirective } from '../../shared/directives/dynamic-translation.directive';
 
 @Component({
   selector: 'app-new-user-success-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatExpansionModule,
+    MatButtonModule,
+    TranslateModule,
+    TranslateToolbarComponent,
+    DynamicTranslationDirective
+  ],
   templateUrl: './new-user-success-page.component.html',
   styleUrl: './new-user-success-page.component.scss'
 })
 export class NewUserSuccessPageComponent {
 
+  panelOpenState = false;
+
+  constructor(
+    private router: Router,
+    private translate: TranslateService) {}
+
+    navigateToLogin() {
+      this.router.navigate(['./login']);
+    }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
